7161304e442605eb7741adeeac019407afe96830
sharshil04 <sharshil04@gmail.com>
2020-08-03 13:01:13 +0530

    My Commit

---
+++ js/loader.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    $('.modal').modal();
+    $('.parallax').parallax();
+    $('.sidenav').sidenav();
+    $(".slider").slider({ full_width:true });
+    $(".myreviews").carousel({
+        numVisible: 7,
+        shift: 55,
+        padding:55,
+    });
+})
+
+function toggleModal(){
+
+    var instance = M.modal.getInstance($('modal3'))
+    instance.open();
+}
+
+
+
